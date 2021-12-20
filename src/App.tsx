03dd8aaa91030c02799438e387919dd2b57e3f85
Michael Chau <Michaelwillchau@gmail.com>
2021-12-20 14:05:36 -0800

    init navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
-import { LoginComponent } from './components/LoginComponent';
 import { stringify } from 'querystring';
+import { Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { LoginComponent } from './components/LoginComponent';
 import {Principal} from './models/Principal';
 import { DashboardComponent } from './components/DashboardComponent';
 import PriceComponent from './components/PriceComponent';
+import NavComponent from './components/NavbarComponent';
 
 function App() {
 
   let [authUser, setAuthUser] = useState(undefined as Principal | undefined);
 
   return (
-    <Routes>
-        <Route path="/login" element={<LoginComponent currentUser={authUser} setCurrentUser={setAuthUser} />} />
-        <Route path="/dashboard" element={<DashboardComponent currentUser={authUser}/>}/>
-        <Route path="/price" element={<PriceComponent/>}/>
-    </Routes>
+ 
+    <div className="App">
+      <NavComponent/>
+      <Routes>
+          <Route path="/login" element={<LoginComponent currentUser={authUser} setCurrentUser={setAuthUser} />} />
+          <Route path="/dashboard" element={<DashboardComponent currentUser={authUser}/>}/>
+          <Route path="/price" element={<PriceComponent/>}/>
+      </Routes>
+
+    </div>
+
   );
 }
 
