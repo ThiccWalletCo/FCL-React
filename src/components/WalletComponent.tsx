9d791e98f54ad342c4b0a9c75e2dd9d02dc88b50
Andrew Aslakson <andrew01177@gmail.com>
2021-12-23 00:57:13 -0500

    Implemented closing websocket

--- src/components/WalletComponent.tsx
+++ src/components/WalletComponent.tsx
@@ -7,6 +7,7 @@ import { WalletRequest } from '../models/WalletRequest';
 import { Principal } from '../models/Principal';
 import { TransactionComponent } from './TransactionComponent';
 import CoinWallet from '../models/CoinWallet'
+import { Socket } from 'dgram';
 
 interface IWalletProps{
     currWallet: WalletRequest | undefined,
@@ -25,6 +26,12 @@ export default function WalletContents(props:IWalletProps) {
     let count = numIterations;
     let first = true;
     let tempWalletCoinList: CoinWallet[];
+
+    let prices:number[] = []; 
+            let averages:number[] = [];
+            let sums:number[] = [];
+            let counts:number[] = [];
+            let firsts:boolean[] = [];
 
     useEffect( () => {
             console.log(props.currWallet);
@@ -50,136 +57,138 @@ export default function WalletContents(props:IWalletProps) {
            
         }
        
-    }, []);
-
-    function sock() {
+    
         let socket = new WebSocket("wss://ws-feed.exchange.coinbase.com/");
-
-
-
-        let request: {type: string, 
-                    product_ids: string[],
-                    channels: (string|{
-                        name: string,
-                        product_ids: string[]
-                    })[]} = {
-            "type": "subscribe",
-            "product_ids": [
-                //currency pairs are inserted here
-            ],
-            "channels": [
-                "level2",
-                "heartbeat"
-            ]
-        };
-
-        let response: {pair: string, price: number}[] = [];
-
-        let heartbeat: {name:string, product_ids:string[]} = {
-            "name": "ticker",
-            "product_ids": [
-                //"ETH-USD"
-            ]
-        }
-
-        heartbeat.product_ids = pairs; 
-        request.product_ids = pairs; 
-        request.channels.push(heartbeat);
-        let prices:number[] = []; 
-        let averages:number[] = [];
-        let sums:number[] = [];
-        let counts:number[] = [];
-        let firsts:boolean[] = [];
-
-
-        for(let i = 0; i < pairs.length; i++){
-            averages[i] = 0;
-            counts[i] = 0;
-            sums[i] = 0;
-            firsts[i] = true;
-        }
-
-        socket.onopen = function(e) {
-            console.log("[open] Connection established");
-            console.log("Sending to server");
-            socket.send(JSON.stringify(request));
-
-
-        };
-
-        socket.onmessage = function(event) {
-            let jsonObj = JSON.parse(event.data);
-
-
-
-            storeAvgPrices(jsonObj);
+        function sock() {
             
-            if (count > totalNumIterations || first) {
-                count = 0;
-                if(first) {
-                    console.log(averages[0])
-                }
-                first = false;
-                for (let i = 0; i < averages.length; i++) {
-                    tempWalletCoinList[i] = new CoinWallet(pairs[i], amounts[i], averages[i]);
-                    console.log(tempWalletCoinList[i]);
-                }
-                updateWalletCoinList(tempWalletCoinList);
-                console.log(tempWalletCoinList)
+
+
+
+            let request: {type: string, 
+                        product_ids: string[],
+                        channels: (string|{
+                            name: string,
+                            product_ids: string[]
+                        })[]} = {
+                "type": "subscribe",
+                "product_ids": [
+                    //currency pairs are inserted here
+                ],
+                "channels": [
+                    "level2",
+                    "heartbeat"
+                ]
+            };
+
+            let response: {pair: string, price: number}[] = [];
+
+            let heartbeat: {name:string, product_ids:string[]} = {
+                "name": "ticker",
+                "product_ids": [
+                    //"ETH-USD"
+                ]
             }
-            count++;
 
-        };
+            heartbeat.product_ids = pairs; 
+            request.product_ids = pairs; 
+            request.channels.push(heartbeat);
+            
 
-        socket.onclose = function(event) {
-        if (event.wasClean) {
-            console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-        } else {
-            console.log('[close] Connection died');
+
+            for(let i = 0; i < pairs.length; i++){
+                averages[i] = 0;
+                counts[i] = 0;
+                sums[i] = 0;
+                firsts[i] = true;
+            }
+
+            socket.onopen = function(e) {
+                console.log("[open] Connection established");
+                console.log("Sending to server");
+                socket.send(JSON.stringify(request));
+
+
+            };
+
+            socket.onmessage = function(event) {
+                let jsonObj = JSON.parse(event.data);
+
+                console.log('socket is open');
+
+                storeAvgPrices(jsonObj);
+                
+                if (count > totalNumIterations || first) {
+                    count = 0;
+                    if(first) {
+                        console.log(averages[0])
+                    }
+                    first = false;
+                    for (let i = 0; i < averages.length; i++) {
+                        tempWalletCoinList[i] = new CoinWallet(pairs[i], amounts[i], averages[i]);
+                        console.log(tempWalletCoinList[i]);
+                    }
+                    updateWalletCoinList(tempWalletCoinList);
+                    console.log(tempWalletCoinList)
+                }
+                count++;
+
+            };
+
+            socket.onclose = function(event) {
+            if (event.wasClean) {
+                console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            } else {
+                console.log('[close] Connection died');
+            }
+            };
+            socket.onerror = function(error) {
+                console.log(`[error] ${error}`);
+            };
+
+            function storeAvgPrices (jsonObj: any) {
+                tempWalletCoinList = [];
+                if(jsonObj["type"] === "l2update"){
+                    for(let i = 0; i < pairs.length; i++){
+                        if(jsonObj["product_id"]===pairs[i]){
+                            prices[i] = parseFloat(jsonObj["changes"][0][1]);
+                        }
+                        else if(counts[i]>numIterations || firsts[i]){
+
+                            if(firsts[i]){
+                                first = true;
+                                sums[i] = prices[i];
+                                counts[i] = 1;
+                                console.log(sums[i]);
+                                console.log(counts[i]);
+                            }
+                            averages[i] = sums[i]/counts[i];
+                            if(!isNaN(averages[i])){
+                                firsts[i] = false;
+                                let num = averages[i] * amounts[i];//calculate how much league user has
+                                averages[i] = Math.round((num+Number.EPSILON)*100)/100;//round to hundrendth decimal place                    
+                            }
+                            counts[i] = 0;
+                            sums[i] = 0;
+                        }
+                        else {
+                            sums[i] += prices[i]; 
+                            counts[i]++;        
+                        }
+                    }
+                    
+                    
+                }
+
+                return response;
         }
-        };
-        socket.onerror = function(error) {
-            console.log(`[error] ${error}`);
-        };
-
-        function storeAvgPrices (jsonObj: any) {
-            tempWalletCoinList = [];
-            if(jsonObj["type"] === "l2update"){
-                for(let i = 0; i < pairs.length; i++){
-                    if(jsonObj["product_id"]===pairs[i]){
-                        prices[i] = parseFloat(jsonObj["changes"][0][1]);
-                    }
-                    else if(counts[i]>numIterations || firsts[i]){
-
-                        if(firsts[i]){
-                            first = true;
-                            sums[i] = prices[i];
-                            counts[i] = 1;
-                            console.log(sums[i]);
-                            console.log(counts[i]);
-                        }
-                        averages[i] = sums[i]/counts[i];
-                        if(!isNaN(averages[i])){
-                            firsts[i] = false;
-                            let num = averages[i] * amounts[i];//calculate how much league user has
-                            averages[i] = Math.round((num+Number.EPSILON)*100)/100;//round to hundrendth decimal place                    
-                        }
-                        counts[i] = 0;
-                        sums[i] = 0;
-                    }
-                    else {
-                        sums[i] += prices[i]; 
-                        counts[i]++;        
-                    }
-                }
-                
-                
-            }
-
-            return response;
-    }
 
     }
+    return () => {
+        console.log('Socket is closing');
+        
+        socket.close();
+    }
+}, []);
 
     return (
         <>
